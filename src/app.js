3fe50d693683582bd58d8b85b90df3630d88c8e1
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-03-21 22:08:44 +0530

    set up the app and some middlewares

---
+++ src/app.js
@@ -0,0 +1,31 @@
+//writing express code
+import express from 'express';
+import cookieParser from 'cookie-parser'; //importing cookieparser here 
+import cors from 'cors'; //importing cors 
+
+const app = express();
+
+//using cors here using middleares >> and we are configuring cors by passing object to it and giving it origin in .env CORS_ORIGN=* is allowing request from anywhere which is not a god practice.
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+//accpeting json into our server and implementing using middleware from express.json
+app.use(express.json({ limit: "20kb" })); //giving limit to 20kb to  accpet json in our app 
+
+//configuration to our app for taking value from the params we going to use urlencoded
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+//for storing /serving static file we have to use static into our middlewares
+app.use(express.static("public"));
+
+//using cookie parser
+app.use(cookieParser());
+
+
+app.get("/", (req, res) => {
+    res.send("Hello backend");
+});
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from 'dotenv'; //using import statement
 import connectDB from './db/index.js'
+import { app } from './app.js';
 dotenv.config({
     path: './env'
 });
 
-
-connectDB();
+//writing promise method if database is connected then our app will listen on some port ortherwise throw error
+connectDB()
+    .then(() => {
+        //if our app have some error then it will
+        app.on("error", (error) => {
+            console.log(`Server is facing some error: ${error}`);
+        });
+        //our app will listen
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`our app is listening on PORT: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log('MongoDB connection failed!!', error);
+    });
 
 
 
