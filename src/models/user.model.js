6c133524cf193586e457b90e0b0da5442bb4ba7d
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-03-22 22:10:59 +0530

    video models and used bcrypt to hash our user password before saving their information to DB

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 //making user models here now we are making structure of our model in which our data is stored in databses
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
     {
@@ -45,6 +47,32 @@ const userSchema = new mongoose.Schema(
             type: String,
         }
     }, { timestamps: true }); //createdAt and updatedAt will be get by the timpestamps in 2nd object
+
+//using pre middleware to just hash our password before storing userdata to our database.
+// we have to use normal annomous function and we cannot use our normal arrow function as arrow function does noit have access to this keyword, we have to give coontext to our userSchema
+// as this is middleware and it will take some time to  do some task so this is why we use async and middleware have next just to pass flag to next.
+userSchema.pre("save", async function (next) {
+
+    //we are solving one problem if we change/modify some data then this pre wil always run and hash our password on every modification so we have to avoid it
+    //avoiding re-hasing of password
+    //agr password modify nhi hua to next return krdo
+    if (!this.isModified("password")) return next();
+
+    //hassing our password
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+
+//user ko jb bhi import karaye toh user se phuch le ki password sahi hai ya nahi kyuki DB mei hashed password hai and user nei normal 'user@122' use kiya hua ho skta hai.
+//defining our custom methods
+//its just like we do it in js with prototype writing our custom method
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password); //compare take 2 parameter 1st -> is password which user will give and 2nd -> is our hashed password.
+} //this function will return true or false
+
+
+
 
 
 export const User = mongoose.model('User', userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,7 +1,44 @@
 //making video model here in this model file
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; //importing mongoose aggregate paginate its just like a plugin.
 
-const videoSchema = new Schema({}, { timestamps: true });
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudnary url
+            required: true,
+        },
+        thumnail: {
+            type: String, // cloudnary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        view: {
+            type: Number,
+            default: 0 //giving default 0 to views
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, { timestamps: true });
 
+//we can write our own plugin >> this aggregation is used to write complex querries
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model('Video', videoSchema);
