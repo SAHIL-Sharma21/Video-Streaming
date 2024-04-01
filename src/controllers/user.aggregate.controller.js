00ce0f87047fde9e6779ce2f59e296151d53ddad
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-01 13:11:48 +0530

    makeing route full

--- src/controllers/user.aggregate.controller.js
+++ src/controllers/user.aggregate.controller.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from '../models/user.model.js'
-import mongoose from "mongoose";
+
 
 const getUserChannelProfile = asyncHandler(async (req, res) => {
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,8 +4,9 @@
 import { Router } from "express";
 import { registerUser } from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'; //importing multer middleware 
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken } from '../controllers/user.login.controller.js'
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, updateAccountDetails, updateAvatar, updateCoverImage } from '../controllers/user.login.controller.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { getUserChannelProfile, getWatchHistory } from "../controllers/user.aggregate.controller.js";
 
 const router = Router();
 
@@ -32,11 +33,22 @@ router.route("/logout").post(verifyJWT, logoutUser);
 //route to refresh a access token and refresh token
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+//get currentuser
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+//update account details
+//patch use krna padega wrna poorqa field hi change kr dega agr hm post use krenge toh
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+//avatar update
+//patch uise krna padega -->> only ek avatar update kr rahe hia
+//using multer middleware to upload single file 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+//making update coverImage route
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
 
-
-
-
-
+//using params to get username for making route for getinguserProfile
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+//getting watch history
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 //exporting this router as a default
 export default router;
