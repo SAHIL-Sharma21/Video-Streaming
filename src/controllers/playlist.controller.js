32596db06d845bb6b0e54a8a3c97272384bea6f1
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-05 14:20:11 +0530

    playlist 2 controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -10,11 +10,85 @@ const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body
 
     //TODO: create playlist
+    //creating playlist fro the loggedIn user
+
+    if (!name || !description) {
+        throw new ApiError(400, "Playlist name and description is required");
+    }
+
+    //creating playlist
+    const createdPlaylist = await Playlist.create({
+        name,
+        description,
+        videos: [],
+        owner: req.user?._id,
+    });
+
+    if (!createdPlaylist) {
+        throw new ApiError(500, "Error while creating a playlist.");
+    }
+
+    return res
+        .status(201)
+        .json(new ApiResponse(200, createdPlaylist, "Playlist created successfully!"));
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const { userId } = req.params
     //TODO: get user playlists
+    //getting user playlist from its userId which is getting us from params
+    if (!userId) {
+        throw new ApiError(400, "User Id is required to get the playlist");
+    }
+
+    //finding the user amd then we will lookup to his playlist and count the total playlist he have
+    const userPlaylist = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "user"
+            }
+        },
+        {
+            $group: {
+                _id: "$owner",
+                totalPlaylist: { $sum: 1 },
+                playlist: { $push: "$$ROOT" }, //push the playlist into an array
+                user: {
+                    $first: {
+                        $arrayElemAt: ["$user", 0]//get the user detail
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                totalPlaylist: 1,
+                user: {
+                    userName: 1,
+                    fullname: 1,
+                    avatar: 1
+                },
+                // playlist: 1
+            }
+        }
+    ]);
+
+    if (!userPlaylist || userPlaylist.length === 0) {
+        throw new ApiError(400, "user doesn't have the playlist.")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, userPlaylist[0], "user playlist fetched successfully."));
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
