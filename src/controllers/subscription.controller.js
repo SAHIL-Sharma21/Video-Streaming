558bc547b18ce41e9e19f9849ae193452e3cd376
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-06 15:47:04 +0530

    Subscribtion controller 1 functionality done

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -10,7 +10,48 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
     // TODO: toggle subscription
-})
+
+    if (!channelId) {
+        throw new ApiError(400, "Channel ID is required.")
+    }
+
+    //we wil check if that user is a subscriber of that channel or not
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(400, "Please login to perfoem subscription.")
+    }
+
+    //finding that user is subscribed to that channel or not 
+    const subscriber = await Subscription.findOne({
+        subscriber: userId,
+        channel: channelId
+    });
+
+    if (!subscriber) {
+        try { // if user didnot subscribed then we will subscribe him and making an database entry!
+            const subscribingChannel = await Subscription.create({
+                subscriber: userId,
+                channel: channelId,
+            });
+            return res
+                .status(201)
+                .json(new ApiResponse(200, subscribingChannel, "User subscribed to channel successfully!"));
+        } catch (error) {
+            throw new ApiError(500, "Error while subscribing the channel.")
+        }
+
+    } else {
+        try { //is user is already subscribed the we will unsubscribed him adn delte the entry from database.
+            const removeSubscriber = await Subscription.findOneAndDelete({ subscriber: userId, channel: channelId });
+            return res
+                .status(201)
+                .json(new ApiResponse(200, removeSubscriber, "Chanel unsubscribed successfully!"));
+        } catch (error) {
+            throw new ApiError(500, "Unable to unsubscibe the channel.");
+        }
+    }
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
